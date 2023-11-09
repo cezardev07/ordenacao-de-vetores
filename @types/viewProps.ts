c388353f94c4d7f45326bae8a8d10a49b63a9a1d
cezardev07 <cezardev07@gmail.com>
2023-11-09 11:28:11 -0300

    feat: add @types view

---
+++ @types/viewProps.ts
@@ -0,0 +1,8 @@
+import { vetorProps } from "./vetorProps";
+// importa o tipo para o method bubbleSort
+
+export type viewProps = {
+  BubbleSort?: vetorProps;
+  SelectionSort?: vetorProps;
+  InsertionSort?: vetorProps;
+};
