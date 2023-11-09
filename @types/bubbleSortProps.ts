c30340706f5370d9b75aac23d141fcccc971cd9d
cezardev07 <cezardev07@gmail.com>
2023-11-09 11:27:30 -0300

    feat: add @types bubble sort

---
+++ @types/bubbleSortProps.ts
@@ -0,0 +1,6 @@
+import { vetorProps } from "./vetorProps";
+// importa o tipo para o method bubbleSort
+
+export interface bubbleSortProps{
+  bubbleSort: (vetor: vetorProps) => vetorProps;
+}; // Declara um tipo para o method bubbleSort
