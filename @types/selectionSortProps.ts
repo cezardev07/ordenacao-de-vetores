f105df7b9c25cd1a0de04eb15a3b2b4b2e68d693
cezardev07 <cezardev07@gmail.com>
2023-11-09 11:30:36 -0300

    feat: add @types selection sort

---
+++ @types/selectionSortProps.ts
@@ -0,0 +1,6 @@
+import { vetorProps } from "./vetorProps";
+// importa o tipo para o method bubbleSort
+
+export interface SelectionSortProps {
+  selectionSort: (vetor: vetorProps) => vetorProps;
+};
